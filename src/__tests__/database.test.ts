b8dacee9875d11aec637d8ea37fe3bd598722534
albert <albert@air.local>
2025-07-28 11:53:38 +0200

    adds database class and basic tests for jest

---
+++ src/__tests__/database.test.ts
@@ -0,0 +1,6 @@
+// test temporal para probar que jest funciona
+describe('DatabaseConnection', () => {
+    describe('Singleton Pattern', () => {
+        test('should return the same instance', () => {})
+    })
+})
