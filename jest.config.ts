5be3908ccfe041b756f7fee34f43f3a37c5e2490
albert <albert@air.local>
2025-07-28 23:05:37 +0200

    updates jest.config.ts and tsconfig.json to get dependencies on tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,12 +3,14 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
   extensionsToTreatAsEsm: ['.ts'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+    '^.+\.ts$': ['ts-jest', { useESM: true }],
   },
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^(\.{1,2}/.*)\.js$': '$1',
   },
 };
 
