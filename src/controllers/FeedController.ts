f2acbad1fabaf300eadf64b397805dead4177b06
albert <albert@air.local>
2025-07-28 11:52:57 +0200

    add "Feed" files with explanation to plan some basic file structure

---
+++ src/controllers/FeedController.ts
@@ -0,0 +1,6 @@
+// Aquí exportamos una classe que gestiona las peticiones y comunica con los servicios/repositorioes
+// thinking out loud: creo que voy a usar hono.dev, usaba express hace tiempo, y veo que es una especie de express moderno compatible con express
+
+export class FeedController {
+
+}
